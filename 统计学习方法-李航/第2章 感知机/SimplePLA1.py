c7da1a129439af80834d8d8d5bff415dce254dac
LittleSheepy <38182200+LittleSheepy@users.noreply.github.com>
2019-04-16 23:04:25 +0800

    1
    1

--- 统计学习方法-李航/第2章 感知机/SimplePLA1.py
+++ 统计学习方法-李航/第2章 感知机/SimplePLA1.py
@@ -1,30 +1,44 @@
 import numpy as np
 from sklearn.datasets import make_blobs
 import matplotlib.pyplot as plt
+import copy
+from pltGif import pltGif
 
 # 创建一个数据集，X有两个特征，y={-1，1}
-X, y = make_blobs(n_samples=500, centers=2, random_state=6)
-y[y==0] = -1
+PLA_NUM = 50
+PLA_NUM_TEST = 2
+X, y = make_blobs(n_samples=PLA_NUM, centers=2, random_state=6)
+y[y==1] = -1
+y[y==0] = 1
 plt.scatter(X[:, 0], X[:, 1], c=y, s=30, cmap=plt.cm.Paired)
 plt.xlabel("feature_1")
 plt.ylabel("feature_2")
 plt.show()
-
+history = []  # 用来记录每次更新过后的w,b
+#history.append([copy.copy(w), b])  # 将每次更新过后的w,b记录在history数组中
+hisDot = []
+hisL = []
 
 class PerceptronRaw():
     def __init__(self):
         self.W = None;
         self.bias = None;
+        self.history = []  # 用来记录每次更新过后的w,b
+        self.hisDot = []
 
     def fit(self, x_train, y_train, learning_rate=0.05, n_iters=100, plot_train=True):
         print("开始训练...")
         num_samples, num_features = x_train.shape
-        self.W = np.random.randn(num_features)
-        self.bias = 0
+        self.W = np.array([-0.1, 0.1])
+        self.bias = 1
+        self.history.append([copy.copy(self.W), self.bias])
 
         while True:
             erros_examples = []
             erros_examples_y = []
+            distanceSum = 0
+
+            w_ = np.sqrt(np.sum(np.array(self.W) ** 2))
             # 查找错误分类的样本点
             for idx in range(num_samples):
                 example = x_train[idx]
@@ -34,6 +48,8 @@ class PerceptronRaw():
                 if distance <= 0:
                     erros_examples.append(example)
                     erros_examples_y.append(y_idx)
+                    distanceSum += -distance/w_
+            print(distanceSum)
             if len(erros_examples) == 0:
                 break;
             else:
@@ -42,7 +58,9 @@ class PerceptronRaw():
                 choosed_example = erros_examples[random_idx]
                 choosed_example_y = erros_examples_y[random_idx]
                 self.W = self.W + learning_rate * choosed_example_y * choosed_example
-                self.bias = self.bias + learning_rate * choosed_example_y
+                #self.bias = self.bias + learning_rate * choosed_example_y
+                self.history.append([copy.copy(self.W), self.bias])
+                self.hisDot.append(choosed_example)
         print("训练结束")
 
         # 绘制训练结果部分
@@ -68,10 +86,10 @@ class PerceptronRaw():
         y_predict = np.sign(np.dot(x, self.W) + self.bias)
         return y_predict
 
-X_train = X[0:450]
-y_train = y[0:450]
-X_test = X[450:500]
-y_test = y[450:500]
+X_train = X[0:(PLA_NUM-PLA_NUM_TEST)]
+y_train = y[0:(PLA_NUM-PLA_NUM_TEST)]
+X_test = X[(PLA_NUM-PLA_NUM_TEST):PLA_NUM]
+y_test = y[(PLA_NUM-PLA_NUM_TEST):PLA_NUM]
 
 # 实例化模型，并训练
 model_raw = PerceptronRaw()
@@ -83,3 +101,6 @@ y_predict = model_raw.predict(X_test)
 
 accuracy = np.sum(y_predict == y_test) / y_predict.shape[0]
 print("原始形式模型在测试集上的准确率: {0}".format(accuracy))
+
+pltGif_ = pltGif(np.column_stack((X, y)), model_raw.history, model_raw.hisDot)
+pltGif_.save()
