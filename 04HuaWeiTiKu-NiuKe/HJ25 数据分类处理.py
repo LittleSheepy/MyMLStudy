d658e5269d715bec39ca961254d59ccd523e8617
LittleSheepy <1165811567@qq.com>
2022-10-27 19:19:37 +0800

    1
    1

--- 04HuaWeiTiKu-NiuKe/HJ25 数据分类处理.py
+++ 04HuaWeiTiKu-NiuKe/HJ25 数据分类处理.py
@@ -1,21 +1,25 @@
-while True:
-    try:
-        a=input().split()[1:]
-        b=map(str,sorted(map(int,set(input().split()[1:]))))
-        totalNum=0
-        res=""
-        for num in b:
-            singleRes,count="",0
-            for i,v in enumerate(a):
-                if num in v:
-                    singleRes+=str(i)+" "+v+" "
-                    totalNum+=2
-                    count+=1
-            if count:
-                singleRes=num+" "+str(count)+" "+singleRes
-                totalNum+=2
-            res+=singleRes
-        print((str(totalNum)+" "+res).rstrip())
+# a=input().split()[1:]
+# b = input()
+b = '5 6 3 6 3 0'
+b = set(b.split()[1:])
+b = map(int,b)
+b = sorted(b)
+b=map(str, b)
+b = list(b)
+a = ['123', '456', '786', '453', '46', '7', '5', '3', '665', '453456', '745', '456', '786', '453', '123']
+totalNum=0
+res=""
+for num in b:
+    print('> ',num)
+    singleRes,count="",0
+    for i,v in enumerate(a):
+        if num in v:
+            singleRes+=str(i)+" "+v+" "
+            totalNum+=2
+            count+=1
+    if count:
+        singleRes=num+" "+str(count)+" "+singleRes
+        totalNum+=2
+    res+=singleRes
+print((str(totalNum)+" "+res).rstrip())
 
-    except:
-        break
