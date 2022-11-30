e1c9a9da40cc1320d6cbf13dbf51d126dbc49b9f
LittleSheepy <1165811567@qq.com>
2022-11-30 11:09:36 +0800

    1
    1

--- 07TensorRTStudy/01TRTexample/src/02hello-inference.cpp
+++ 07TensorRTStudy/01TRTexample/src/02hello-inference.cpp
@@ -18,26 +18,6 @@ using namespace std;
 
 int build_model();
 
-
-vector<unsigned char> load_file(const string& file){
-    ifstream in(file, ios::in | ios::binary);
-    if (!in.is_open())
-        return {};
-
-    in.seekg(0, ios::end);
-    size_t length = in.tellg();
-
-    std::vector<uint8_t> data;
-    if (length > 0){
-        in.seekg(0, ios::beg);
-        data.resize(length);
-
-        in.read((char*)&data[0], length);
-    }
-    in.close();
-    return data;
-}
-
 void inference(){
 
     // ------------------------------ 1. 准备模型并加载   ----------------------------
--- 07TensorRTStudy/01TRTexample/src/common.cpp
+++ 07TensorRTStudy/01TRTexample/src/common.cpp
@@ -8,6 +8,24 @@ nvinfer1::Weights make_weights(float* ptr, int n) {
     return w;
 }
 
+vector<unsigned char> load_file(const string& file) {
+    ifstream in(file, ios::in | ios::binary);
+    if (!in.is_open())
+        return {};
+
+    in.seekg(0, ios::end);
+    size_t length = in.tellg();
+
+    std::vector<uint8_t> data;
+    if (length > 0) {
+        in.seekg(0, ios::beg);
+        data.resize(length);
+
+        in.read((char*)&data[0], length);
+    }
+    in.close();
+    return data;
+}
 void TRTLogger::log(Severity severity, nvinfer1::AsciiChar const* msg) noexcept {
     if (severity <= Severity::kVERBOSE) {
         printf("=%d: %s\n", severity, msg);
