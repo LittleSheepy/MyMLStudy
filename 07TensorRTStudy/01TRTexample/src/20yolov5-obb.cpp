4d9661b45d7486afd867c3fc336363da46c7c681
LittleSheepy <1165811567@qq.com>
2022-12-02 07:04:58 +0800

    1
    1

---
+++ 07TensorRTStudy/01TRTexample/src/20yolov5-obb.cpp
@@ -0,0 +1,333 @@
+
+// tensorRT include
+// 编译用的头文件
+#include <NvInfer.h>
+
+// onnx解析器的头文件
+#include <onnx-tensorrt/NvOnnxParser.h>
+
+// 推理用的运行时头文件
+#include <NvInferRuntime.h>
+
+// cuda include
+#include <cuda_runtime.h>
+
+// system include
+#include <stdio.h>
+#include <math.h>
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <memory>
+#include <functional>
+#ifdef WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
+#include <opencv2/opencv.hpp>
+#include "box_iou_rotated_utils.h"
+#include "common.h"
+
+using namespace std;
+
+static const char* cocolabels[] = {
+    "person", "bicycle", "car", "motorcycle", "airplane",
+    "bus", "train", "truck", "boat", "traffic light", "fire hydrant",
+    "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse",
+    "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack",
+    "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis",
+    "snowboard", "sports ball", "kite", "baseball bat", "baseball glove",
+    "skateboard", "surfboard", "tennis racket", "bottle", "wine glass",
+    "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich",
+    "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake",
+    "chair", "couch", "potted plant", "bed", "dining table", "toilet", "tv",
+    "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave",
+    "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase",
+    "scissors", "teddy bear", "hair drier", "toothbrush"
+};
+
+// 通过智能指针管理nv返回的指针参数
+// 内存自动释放，避免泄漏
+template<typename _T>
+shared_ptr<_T> make_nvshared(_T* ptr){
+    return shared_ptr<_T>(ptr, [](_T* p){p->destroy();});
+}
+
+// 上一节的代码
+bool build_model20(){
+
+    if(exists("best.trtmodel")){
+        printf("best.trtmodel has exists.\n");
+        return true;
+    }
+
+    TRTLogger logger;
+
+    // 这是基本需要的组件
+    auto builder = make_nvshared(nvinfer1::createInferBuilder(logger));
+    auto config = make_nvshared(builder->createBuilderConfig());
+
+    // createNetworkV2(1)表示采用显性batch size，新版tensorRT(>=7.0)时，不建议采用0非显性batch size
+    // 因此贯穿以后，请都采用createNetworkV2(1)而非createNetworkV2(0)或者createNetwork
+    auto network = make_nvshared(builder->createNetworkV2(1));
+
+    // 通过onnxparser解析器解析的结果会填充到network中，类似addConv的方式添加进去
+    auto parser = make_nvshared(nvonnxparser::createParser(*network, logger));
+    if(!parser->parseFromFile("best.onnx", 1)){
+        printf("Failed to parse best.onnx\n");
+
+        // 注意这里的几个指针还没有释放，是有内存泄漏的，后面考虑更优雅的解决
+        return false;
+    }
+    
+    int maxBatchSize = 10;
+    printf("Workspace Size = %.2f MB\n", (1 << 28) / 1024.0f / 1024.0f);
+    config->setMaxWorkspaceSize(1 << 28);
+
+    // 如果模型有多个输入，则必须多个profile
+    auto profile = builder->createOptimizationProfile();
+    auto input_tensor = network->getInput(0);
+    auto input_dims = input_tensor->getDimensions();
+    
+    // 配置最小允许batch
+    input_dims.d[0] = 1;
+    profile->setDimensions(input_tensor->getName(), nvinfer1::OptProfileSelector::kMIN, input_dims);
+    profile->setDimensions(input_tensor->getName(), nvinfer1::OptProfileSelector::kOPT, input_dims);
+
+    // 配置最大允许batch
+    // if networkDims.d[i] != -1, then minDims.d[i] == optDims.d[i] == maxDims.d[i] == networkDims.d[i]
+    input_dims.d[0] = maxBatchSize;
+    profile->setDimensions(input_tensor->getName(), nvinfer1::OptProfileSelector::kMAX, input_dims);
+    config->addOptimizationProfile(profile);
+
+    auto engine = make_nvshared(builder->buildEngineWithConfig(*network, *config));
+    if(engine == nullptr){
+        printf("Build engine failed.\n");
+        return false;
+    }
+
+    // 将模型序列化，并储存为文件
+    auto model_data = make_nvshared(engine->serialize());
+    FILE* f = fopen("best.trtmodel", "wb");
+    fwrite(model_data->data(), 1, model_data->size(), f);
+    fclose(f);
+
+    // 卸载顺序按照构建顺序倒序
+    printf("Build Done.\n");
+    return true;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+void inference20(){
+
+    TRTLogger logger;
+    auto engine_data = load_file("best.trtmodel");
+    auto runtime   = make_nvshared(nvinfer1::createInferRuntime(logger));
+    auto engine = make_nvshared(runtime->deserializeCudaEngine(engine_data.data(), engine_data.size()));
+    if(engine == nullptr){
+        printf("Deserialize cuda engine failed.\n");
+        runtime->destroy();
+        return;
+    }
+
+    if(engine->getNbBindings() != 2){
+        printf("你的onnx导出有问题，必须是1个输入和1个输出，你这明显有：%d个输出.\n", engine->getNbBindings() - 1);
+        return;
+    }
+
+    cudaStream_t stream = nullptr;
+    checkRuntime(cudaStreamCreate(&stream));
+    auto execution_context = make_nvshared(engine->createExecutionContext());
+
+    int input_batch = 1;
+    int input_channel = 3;
+    int input_height = 1024;
+    int input_width = 1024;
+    int input_numel = input_batch * input_channel * input_height * input_width;
+    float* input_data_host = nullptr;
+    float* input_data_device = nullptr;
+    checkRuntime(cudaMallocHost(&input_data_host, input_numel * sizeof(float)));
+    checkRuntime(cudaMalloc(&input_data_device, input_numel * sizeof(float)));
+
+    ///////////////////////////////////////////////////
+    // letter box
+    auto image = cv::imread("P0032.jpg");
+    float scale_x = input_width / (float)image.cols;
+    float scale_y = input_height / (float)image.rows;
+    float scale = std::min(scale_x, scale_y);
+    float i2d[6], d2i[6];
+    i2d[0] = scale;  i2d[1] = 0;  i2d[2] = (-scale * image.cols + input_width + scale  - 1) * 0.5;
+    i2d[3] = 0;  i2d[4] = scale;  i2d[5] = (-scale * image.rows + input_height + scale - 1) * 0.5;
+
+    cv::Mat m2x3_i2d(2, 3, CV_32F, i2d);
+    cv::Mat m2x3_d2i(2, 3, CV_32F, d2i);
+    cv::invertAffineTransform(m2x3_i2d, m2x3_d2i);
+
+    cv::Mat input_image(input_height, input_width, CV_8UC3);
+    cv::warpAffine(image, input_image, m2x3_i2d, input_image.size(), cv::INTER_LINEAR, cv::BORDER_CONSTANT, cv::Scalar::all(114));
+    cv::imwrite("../files/input-image.jpg", input_image);
+
+    int image_area = input_image.cols * input_image.rows;
+    unsigned char* pimage = input_image.data;
+    float* phost_b = input_data_host + image_area * 0;
+    float* phost_g = input_data_host + image_area * 1;
+    float* phost_r = input_data_host + image_area * 2;
+    for(int i = 0; i < image_area; ++i, pimage += 3){
+        // 注意这里的顺序rgb调换了
+        *phost_r++ = pimage[0] / 255.0f;
+        *phost_g++ = pimage[1] / 255.0f;
+        *phost_b++ = pimage[2] / 255.0f;
+    }
+    ///////////////////////////////////////////////////
+    checkRuntime(cudaMemcpyAsync(input_data_device, input_data_host, input_numel * sizeof(float), cudaMemcpyHostToDevice, stream));
+
+    // 3x3输入，对应3x3输出
+    auto output_dims = engine->getBindingDimensions(1);
+    int output_numbox = output_dims.d[1];
+    int output_numprob = output_dims.d[2];
+    int num_classes = output_numprob - 185;
+    int output_numel = input_batch * output_numbox * output_numprob;
+    float* output_data_host = nullptr;
+    float* output_data_device = nullptr;
+    checkRuntime(cudaMallocHost(&output_data_host, sizeof(float) * output_numel));
+    checkRuntime(cudaMalloc(&output_data_device, sizeof(float) * output_numel));
+
+    // 明确当前推理时，使用的数据输入大小
+    auto input_dims = engine->getBindingDimensions(0);
+    input_dims.d[0] = input_batch;
+
+    execution_context->setBindingDimensions(0, input_dims);
+    float* bindings[] = {input_data_device, output_data_device};
+    bool success      = execution_context->enqueueV2((void**)bindings, stream, nullptr);
+    checkRuntime(cudaMemcpyAsync(output_data_host, output_data_device, sizeof(float) * output_numel, cudaMemcpyDeviceToHost, stream));
+    checkRuntime(cudaStreamSynchronize(stream));
+
+    // decode box
+    // cx, cy, w, h, theta, class, conf
+    vector<vector<float>> bboxes;
+    float confidence_threshold = 0.25;
+    float nms_threshold = 0.5;
+    for(int i = 0; i < output_numbox; ++i){
+        float* ptr = output_data_host + i * output_numprob;
+        float objness = ptr[4];
+        if(objness < confidence_threshold)
+            continue;
+
+        float* pclass = ptr + 5;
+        float* ptheta = pclass + num_classes;
+        int label     = std::max_element(pclass, pclass + num_classes) - pclass;
+        float prob    = pclass[label];
+        float confidence = prob * objness;
+        if(confidence < confidence_threshold)
+            continue;
+
+        float theta  = std::max_element(ptheta, ptheta + 180) - ptheta;
+              theta  = (theta - 90.0f) / 180.0f * 3.14159265f;
+
+        float cx     = ptr[0];
+        float cy     = ptr[1];
+        float width  = ptr[2];
+        float height = ptr[3];
+        float left   = cx - width * 0.5;
+        float top    = cy - height * 0.5;
+        float right  = cx + width * 0.5;
+        float bottom = cy + height * 0.5;
+        float image_base_left   = d2i[0] * left   + d2i[2];
+        float image_base_right  = d2i[0] * right  + d2i[2];
+        float image_base_top    = d2i[0] * top    + d2i[5];
+        float image_base_bottom = d2i[0] * bottom + d2i[5];
+        float image_base_cx = (image_base_left + image_base_right)  / 2;
+        float image_base_cy = (image_base_top  + image_base_bottom) / 2;
+        float image_base_width  = image_base_right - image_base_left;
+        float image_base_height = image_base_bottom - image_base_top;
+        bboxes.push_back({image_base_cx, image_base_cy, image_base_width, image_base_height, theta, (float)label, confidence});
+    }
+    printf("decoded bboxes.size = %d, num_classes = %d\n", bboxes.size(), num_classes);
+
+    // nms
+    std::sort(bboxes.begin(), bboxes.end(), [](vector<float>& a, vector<float>& b){return a[6] > b[6];});
+    std::vector<bool> remove_flags(bboxes.size());
+    std::vector<vector<float>> box_result;
+    box_result.reserve(bboxes.size());
+
+    auto iou = [](const vector<float>& a, const vector<float>& b){
+        return single_box_iou_rotated(a.data(), b.data());
+    };
+
+    for(int i = 0; i < bboxes.size(); ++i){
+        if(remove_flags[i]) continue;
+
+        auto& ibox = bboxes[i];
+        box_result.emplace_back(ibox);
+        for(int j = i + 1; j < bboxes.size(); ++j){
+            if(remove_flags[j]) continue;
+
+            auto& jbox = bboxes[j];
+            if(ibox[5] == jbox[5]){
+                // class matched
+                if(iou(ibox, jbox) >= nms_threshold)
+                    remove_flags[j] = true;
+            }
+        }
+    }
+    printf("box_result.size = %d\n", box_result.size());
+
+    for(int i = 0; i < box_result.size(); ++i){
+        auto& ibox = box_result[i];
+        float cx = ibox[0];
+        float cy = ibox[1];
+        float width = ibox[2];
+        float height = ibox[3];
+        float theta = ibox[4];
+        int class_label = ibox[5];
+        float confidence = ibox[6];
+
+        float cosa = cos(theta);
+        float sina = sin(theta);
+        float v1x = width / 2 * cosa;
+        float v1y = -width / 2 * sina;
+        float v2x = -height / 2 * sina;
+        float v2y = -height / 2 * cosa;
+        float p0x = cx + v1x + v2x;
+        float p0y = cy + v1y + v2y;
+        float p1x = cx + v1x - v2x;
+        float p1y = cy + v1y - v2y;
+        float p2x = cx - v1x - v2x;
+        float p2y = cy - v1y - v2y;
+        float p3x = cx - v1x + v2x;
+        float p3y = cy - v1y + v2y;
+
+        cv::Scalar color;
+        tie(color[0], color[1], color[2]) = random_color(class_label);
+        //cv::rectangle(image, cv::Point(left, top), cv::Point(right, bottom), color, 3);
+        cv::polylines(image, vector<cv::Point>{
+            cv::Point(p0x, p0y), cv::Point(p1x, p1y), cv::Point(p2x, p2y), cv::Point(p3x, p3y)
+        }, true, color, 3, 16);
+
+        auto name      = cocolabels[class_label];
+        auto caption   = cv::format("%s %.2f", name, confidence);
+        int text_width = cv::getTextSize(caption, 0, 1, 2, nullptr).width + 10;
+        cv::rectangle(image, cv::Point(cx-3, cy-33), cv::Point(cx + text_width, cy), color, -1);
+        cv::putText(image, caption, cv::Point(cx, cy-5), 0, 1, cv::Scalar::all(0), 2, 16);
+    }
+    cv::imwrite("../files/image-draw.jpg", image);
+
+    checkRuntime(cudaStreamDestroy(stream));
+    checkRuntime(cudaFreeHost(input_data_host));
+    checkRuntime(cudaFreeHost(output_data_host));
+    checkRuntime(cudaFree(input_data_device));
+    checkRuntime(cudaFree(output_data_device));
+}
+
+int yolov5_obb(){
+    if(!build_model20()){
+        return -1;
+    }
+    inference20();
+    return 0;
+}
