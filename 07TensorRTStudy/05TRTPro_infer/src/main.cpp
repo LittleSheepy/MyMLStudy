6418a99ca46a483dd60c7d3c68e17b4657cf28df
LittleSheepy <1165811567@qq.com>
2022-12-02 23:59:23 +0800

    1
    1

--- 07TensorRTStudy/05TRTPro_infer/src/main.cpp
+++ 07TensorRTStudy/05TRTPro_infer/src/main.cpp
@@ -28,38 +28,15 @@
 #endif
 #include <opencv2/opencv.hpp>
 
+#include "simple-logger.hpp"
 #include "cuda-tools.hpp"
 #include "trt-builder.hpp"
-#include "simple-logger.hpp"
 #include "trt-tensor.hpp"
+#include "trt-infer.hpp"
 #include <Shlwapi.h>
 #pragma comment(lib, "Shlwapi.lib")
 
 using namespace std;
-
-class TRTLogger : public nvinfer1::ILogger{
-public:
-    virtual void log(Severity severity, nvinfer1::AsciiChar const* msg) noexcept override{
-        if(severity <= Severity::kWARNING){
-            if(severity == Severity::kWARNING){
-                INFOW("%s", msg);
-            }
-            else if(severity <= Severity::kERROR){
-                INFOE("%s", msg);
-            }
-            else{
-                INFO("%s", msg);
-            }
-        }
-    }
-} logger;
-
-// 通过智能指针管理nv返回的指针参数
-// 内存自动释放，避免泄漏
-template<typename _T>
-shared_ptr<_T> make_nvshared(_T* ptr){
-    return shared_ptr<_T>(ptr, [](_T* p){p->destroy();});
-}
 
 bool exists(const string& path){
 
@@ -91,26 +68,6 @@ bool build_model(){
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-
-vector<unsigned char> load_file(const string& file){
-    ifstream in(file, ios::in | ios::binary);
-    if (!in.is_open())
-        return {};
-
-    in.seekg(0, ios::end);
-    size_t length = in.tellg();
-
-    std::vector<uint8_t> data;
-    if (length > 0){
-        in.seekg(0, ios::beg);
-        data.resize(length);
-
-        in.read((char*)&data[0], length);
-    }
-    in.close();
-    return data;
-}
-
 vector<string> load_labels(const char* file){
     vector<string> lines;
 
@@ -130,31 +87,18 @@ vector<string> load_labels(const char* file){
 
 void inference(){
 
-    TRTLogger logger;
-    auto engine_data = load_file("engine.trtmodel");
-    auto runtime   = make_nvshared(nvinfer1::createInferRuntime(logger));
-    auto engine = make_nvshared(runtime->deserializeCudaEngine(engine_data.data(), engine_data.size()));
+    auto engine = TRT::load_infer("engine.trtmodel");
     if(engine == nullptr){
         printf("Deserialize cuda engine failed.\n");
-        runtime->destroy();
         return;
     }
 
-    cudaStream_t stream = nullptr;
-    checkRuntime(cudaStreamCreate(&stream));
-    auto execution_context = make_nvshared(engine->createExecutionContext());
+    engine->print();
 
-    int input_batch   = 1;
-    int input_channel = 3;
-    int input_height  = 224;
-    int input_width   = 224;
-    int input_numel   = input_batch * input_channel * input_height * input_width;
-
-    // tensor的建立并不会立即分配内存，而是在第一次需要使用的时候进行分配
-    TRT::Tensor input_data({input_batch, input_channel, input_height, input_width}, TRT::DataType::Float);
-
-    // 为input关联stream，使得在同一个pipeline中执行复制操作
-    input_data.set_stream(stream);
+    auto input       = engine->input();
+    auto output      = engine->output();
+    int input_width  = input->width();
+    int input_height = input->height();
 
     ///////////////////////////////////////////////////
     // image to float
@@ -166,52 +110,27 @@ void inference(){
     cv::resize(image, image, cv::Size(input_width, input_height));
     image.convertTo(image, CV_32F);
 
-    // 利用opencv mat的内存地址引用，实现input与mat的关联，然后利用split函数一次性完成mat到input的复制
     cv::Mat channel_based[3];
     for(int i = 0; i < 3; ++i)
-        // 注意这里 2 - i是实现bgr -> rgb的方式
-        // 这里cpu提供的参数0是表示batch的索引是0，第二个参数表示通道的索引，因此获取的是0, 2-i通道的地址
-        // 而tensor最大的好处就是帮忙计算索引，否则手动计算就得写很多代码
-        channel_based[i] = cv::Mat(input_height, input_width, CV_32F, input_data.cpu<float>(0, 2-i));
+        channel_based[i] = cv::Mat(input_height, input_width, CV_32F, input->cpu<float>(0, 2-i));
 
     cv::split(image, channel_based);
-
-    // 利用opencv的mat操作加速减去均值和除以标准差
     for(int i = 0; i < 3; ++i)
         channel_based[i] = (channel_based[i] / 255.0f - mean[i]) / std[i];
-    
-    // 如果不写，input_data.gpu获取gpu地址时会自动进行复制
-    // 目的就是把内存复制变为隐式进行
-    input_data.to_gpu();
 
-    // 3x3输入，对应3x3输出
-    const int num_classes = 1000;
-    TRT::Tensor output_data({input_batch, num_classes}, TRT::DataType::Float);
-    output_data.set_stream(stream);
+    engine->forward(true);    
 
-    // 明确当前推理时，使用的数据输入大小
-    auto input_dims = execution_context->getBindingDimensions(0);
-    input_dims.d[0] = input_batch;
-
-    execution_context->setBindingDimensions(0, input_dims);
-    float* bindings[] = {input_data.gpu<float>(), output_data.gpu<float>()};
-    bool success      = execution_context->enqueueV2((void**)bindings, stream, nullptr);
-    checkRuntime(cudaStreamSynchronize(stream));
-
-    // 当获取cpu地址的时候，如果数据最新的在gpu上，就进行数据复制，然后再返回cpu地址
-    float* prob = output_data.cpu<float>();
+    int num_classes   = output->size(1);
+    float* prob       = output->cpu<float>();
     int predict_label = std::max_element(prob, prob + num_classes) - prob;
     auto labels = load_labels("../files/labels.imagenet.txt");
     auto predict_name = labels[predict_label];
     float confidence  = prob[predict_label];
     printf("Predict: %s, confidence = %f, label = %d\n", predict_name.c_str(), confidence, predict_label);
-    std::cout << predict_name << std::endl;
-
-    checkRuntime(cudaStreamDestroy(stream));
 }
 
 int main(){
-    
+
     if(!build_model()){
         return -1;
     }
