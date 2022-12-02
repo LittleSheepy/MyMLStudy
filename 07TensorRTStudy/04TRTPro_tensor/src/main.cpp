2105e385c22feff3e1bf268ddb6da02b4ea6ecf6
LittleSheepy <1165811567@qq.com>
2022-12-02 17:37:37 +0800

    1
    1

--- 07TensorRTStudy/04TRTPro_tensor/src/main.cpp
+++ 07TensorRTStudy/04TRTPro_tensor/src/main.cpp
@@ -21,54 +21,28 @@
 #include <vector>
 #include <memory>
 #include <functional>
-#ifdef WIN32
-#include <windows.h>
-#else
 #include <unistd.h>
-#endif
 #include <opencv2/opencv.hpp>
 
 #include "cuda-tools.hpp"
-#include "trt_builder.hpp"
+#include "trt-builder.hpp"
 #include "simple-logger.hpp"
-#include "mix-memory.hpp"
-#include <Shlwapi.h>
-#pragma comment(lib, "Shlwapi.lib")
-using namespace std;
+#include "trt-tensor.hpp"
 
-inline const char* severity_string(nvinfer1::ILogger::Severity t){
-    switch(t){
-        case nvinfer1::ILogger::Severity::kINTERNAL_ERROR: return "internal_error";
-        case nvinfer1::ILogger::Severity::kERROR:   return "error";
-        case nvinfer1::ILogger::Severity::kWARNING: return "warning";
-        case nvinfer1::ILogger::Severity::kINFO:    return "info";
-        case nvinfer1::ILogger::Severity::kVERBOSE: return "verbose";
-        default: return "unknow";
-    }
-}
+using namespace std;
 
 class TRTLogger : public nvinfer1::ILogger{
 public:
     virtual void log(Severity severity, nvinfer1::AsciiChar const* msg) noexcept override{
-        if(severity <= Severity::kINFO){
-            // 打印带颜色的字符，格式如下：
-            // printf("\033[47;33m打印的文本\033[0m");
-            // 其中 \033[ 是起始标记
-            //      47    是背景颜色
-            //      ;     分隔符
-            //      33    文字颜色
-            //      m     开始标记结束
-            //      \033[0m 是终止标记
-            // 其中背景颜色或者文字颜色可不写
-            // 部分颜色代码 https://blog.csdn.net/ericbar/article/details/79652086
+        if(severity <= Severity::kWARNING){
             if(severity == Severity::kWARNING){
-                printf("\033[33m%s: %s\033[0m\n", severity_string(severity), msg);
+                INFOW("%s", msg);
             }
             else if(severity <= Severity::kERROR){
-                printf("\033[31m%s: %s\033[0m\n", severity_string(severity), msg);
+                INFOE("%s", msg);
             }
             else{
-                printf("%s: %s\n", severity_string(severity), msg);
+                INFO("%s", msg);
             }
         }
     }
@@ -164,65 +138,73 @@ void inference(){
     checkRuntime(cudaStreamCreate(&stream));
     auto execution_context = make_nvshared(engine->createExecutionContext());
 
-    int input_batch = 1;
+    int input_batch   = 1;
     int input_channel = 3;
-    int input_height = 224;
-    int input_width = 224;
-    int input_numel = input_batch * input_channel * input_height * input_width;
-    MixMemory input_data;
-    float* input_data_host   = input_data.cpu<float>(input_numel);
-    float* input_data_device = input_data.gpu<float>(input_numel);
+    int input_height  = 224;
+    int input_width   = 224;
+    int input_numel   = input_batch * input_channel * input_height * input_width;
+
+    // tensor的建立并不会立即分配内存，而是在第一次需要使用的时候进行分配
+    TRT::Tensor input_data({input_batch, input_channel, input_height, input_width}, TRT::DataType::Float);
+
+    // 为input关联stream，使得在同一个pipeline中执行复制操作
+    input_data.set_stream(stream);
 
     ///////////////////////////////////////////////////
     // image to float
-    auto image = cv::imread("../files/dog.jpg");
+    auto image = cv::imread("dog.jpg");
     float mean[] = {0.406, 0.456, 0.485};
     float std[]  = {0.225, 0.224, 0.229};
 
     // 对应于pytorch的代码部分
     cv::resize(image, image, cv::Size(input_width, input_height));
-    int image_area = image.cols * image.rows;
-    unsigned char* pimage = image.data;
-    float* phost_b = input_data_host + image_area * 0;
-    float* phost_g = input_data_host + image_area * 1;
-    float* phost_r = input_data_host + image_area * 2;
-    for(int i = 0; i < image_area; ++i, pimage += 3){
-        // 注意这里的顺序rgb调换了
-        *phost_r++ = (pimage[0] / 255.0f - mean[0]) / std[0];
-        *phost_g++ = (pimage[1] / 255.0f - mean[1]) / std[1];
-        *phost_b++ = (pimage[2] / 255.0f - mean[2]) / std[2];
-    }
-    ///////////////////////////////////////////////////
-    checkRuntime(cudaMemcpyAsync(input_data_device, input_data_host, input_numel * sizeof(float), cudaMemcpyHostToDevice, stream));
+    image.convertTo(image, CV_32F);
+
+    // 利用opencv mat的内存地址引用，实现input与mat的关联，然后利用split函数一次性完成mat到input的复制
+    cv::Mat channel_based[3];
+    for(int i = 0; i < 3; ++i)
+        // 注意这里 2 - i是实现bgr -> rgb的方式
+        // 这里cpu提供的参数0是表示batch的索引是0，第二个参数表示通道的索引，因此获取的是0, 2-i通道的地址
+        // 而tensor最大的好处就是帮忙计算索引，否则手动计算就得写很多代码
+        channel_based[i] = cv::Mat(input_height, input_width, CV_32F, input_data.cpu<float>(0, 2-i));
+
+    cv::split(image, channel_based);
+
+    // 利用opencv的mat操作加速减去均值和除以标准差
+    for(int i = 0; i < 3; ++i)
+        channel_based[i] = (channel_based[i] / 255.0f - mean[i]) / std[i];
+    
+    // 如果不写，input_data.gpu获取gpu地址时会自动进行复制
+    // 目的就是把内存复制变为隐式进行
+    input_data.to_gpu();
 
     // 3x3输入，对应3x3输出
     const int num_classes = 1000;
-    MixMemory output_data;
-    float* output_data_host   = output_data.cpu<float>(num_classes);
-    float* output_data_device = output_data.gpu<float>(num_classes);
+    TRT::Tensor output_data({input_batch, num_classes}, TRT::DataType::Float);
+    output_data.set_stream(stream);
 
     // 明确当前推理时，使用的数据输入大小
     auto input_dims = execution_context->getBindingDimensions(0);
     input_dims.d[0] = input_batch;
 
     execution_context->setBindingDimensions(0, input_dims);
-    float* bindings[] = {input_data_device, output_data_device};
+    float* bindings[] = {input_data.gpu<float>(), output_data.gpu<float>()};
     bool success      = execution_context->enqueueV2((void**)bindings, stream, nullptr);
-    checkRuntime(cudaMemcpyAsync(output_data_host, output_data_device, num_classes * sizeof(float), cudaMemcpyDeviceToHost, stream));
     checkRuntime(cudaStreamSynchronize(stream));
 
-    float* prob = output_data_host;
+    // 当获取cpu地址的时候，如果数据最新的在gpu上，就进行数据复制，然后再返回cpu地址
+    float* prob = output_data.cpu<float>();
     int predict_label = std::max_element(prob, prob + num_classes) - prob;
-    auto labels = load_labels("../files/labels.imagenet.txt");
+    auto labels = load_labels("labels.imagenet.txt");
     auto predict_name = labels[predict_label];
     float confidence  = prob[predict_label];
     printf("Predict: %s, confidence = %f, label = %d\n", predict_name.c_str(), confidence, predict_label);
-    std::cout << predict_name << std::endl;
 
     checkRuntime(cudaStreamDestroy(stream));
 }
 
 int main(){
+    
     if(!build_model()){
         return -1;
     }
