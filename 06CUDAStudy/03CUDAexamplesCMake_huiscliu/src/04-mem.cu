14859476dfe53d0019339e091a586f0e20051ab4
LittleSheepy <1165811567@qq.com>
2022-12-04 03:06:06 +0800

    1
    1

---
+++ 06CUDAStudy/03CUDAexamplesCMake_huiscliu/src/04-mem.cu
@@ -0,0 +1,68 @@
+
+#include <stdio.h>
+#include <cuda.h>
+
+typedef double FLOAT;
+
+__global__ void sum(FLOAT *x)
+{
+    int tid = threadIdx.x;
+    printf(">>>>>sumt id = %d\n", tid);
+    x[tid] += 1;
+}
+
+int mem04()
+{
+    int N = 32;
+    int nbytes = N * sizeof(FLOAT);
+
+    FLOAT *dx = NULL, *hx = NULL;
+    int i;
+
+    /* allocate GPU mem */
+    cudaMalloc((void **)&dx, nbytes);
+
+    if (dx == NULL) {
+        printf("couldn't allocate GPU memory\n");
+        return -1;
+    }
+
+    /* alllocate CPU host mem: memory copy is faster than malloc */
+    hx = (FLOAT*)malloc(nbytes);
+    //cuMemAllocHost((void**)&hx, nbytes);
+
+    if (hx == NULL) {
+        printf("couldn't allocate CPU memory\n");
+        return -2;
+    }
+
+    /* init */
+    printf("hx original: \n");
+    for (i = 0; i < N; i++) {
+        hx[i] = i;
+
+        printf("%g\n", hx[i]);
+    }
+
+    /* copy data to GPU */
+    cudaMemcpy(dx, hx, nbytes, cudaMemcpyHostToDevice);
+
+    /* call GPU */
+    sum<<<1, 32>>>(dx);
+
+    /* let GPU finish */
+    cudaDeviceSynchronize();
+
+    /* copy data from GPU */
+    cudaMemcpy(hx, dx, nbytes, cudaMemcpyDeviceToHost);
+
+    printf("\nhx from GPU: \n");
+    for (i = 0; i < N; i++) {
+        printf("%g\n", hx[i]);
+    }
+
+    cudaFree(dx);
+    free(hx);
+
+    return 0;
+}
