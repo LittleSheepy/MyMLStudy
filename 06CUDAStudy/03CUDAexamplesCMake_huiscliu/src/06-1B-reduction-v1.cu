2ced4f2dd8fb47b0246adfd0645f3e1f81bc63d2
LittleSheepy <1165811567@qq.com>
2023-03-05 19:16:01 +0800

    1
    1

--- 06CUDAStudy/03CUDAexamplesCMake_huiscliu/src/06-1B-reduction-v1.cu
+++ 06CUDAStudy/03CUDAexamplesCMake_huiscliu/src/06-1B-reduction-v1.cu
@@ -5,7 +5,146 @@
 #include <stdio.h>
 #include <cuda.h>
 
-typedef double FLOAT;
+typedef float FLOAT;
+// 二叉树算法 规约
+__global__ void reduce0(FLOAT* g_idata, FLOAT* g_odata) {
+    __shared__ FLOAT sdata[256];
+
+    // 
+    unsigned int tid = threadIdx.x;
+    unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    sdata[tid] = g_idata[i];
+    printf(">>>reduce0 tid=%d\n", tid);
+    __syncthreads();
+
+    // do reduction in shared mem
+    for (unsigned int s = 1; s < blockDim.x; s *= 2) {
+        // modulo arithmetic is slow!
+        if ((tid % (2 * s)) == 0) {
+            sdata[tid] += sdata[tid + s];
+            printf(">>>reduce0 tid=%d,sdata[tid]=%f,sdata[tid + s]=%f,\n", tid, sdata[tid], sdata[tid + s]);
+        }
+        __syncthreads();
+    }
+
+    // write result for this block to global mem
+    if (tid == 0) g_odata[0] = sdata[0];
+}
+// 改进
+__global__ void reduce1(FLOAT* g_idata, FLOAT* g_odata) {
+    __shared__ FLOAT sdata[256];
+
+    // 
+    unsigned int tid = threadIdx.x;
+    unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    sdata[tid] = g_idata[i];
+    printf(">>>reduce1 tid=%d\n", tid);
+    __syncthreads();
+
+    // do reduction in shared mem
+    for (unsigned int s = 1; s < blockDim.x; s *= 2) {
+        // modulo arithmetic is slow!
+        int index = 2 * s * tid;
+        if (index < blockDim.x / s)
+        {
+            sdata[index] += sdata[index + s];
+            printf(">>>reduce1 tid=%d,sdata[tid]=%f,sdata[tid + s]=%f,\n", tid, sdata[index], sdata[index + s]);
+        }
+        __syncthreads();
+    }
+
+    // write result for this block to global mem
+    if (tid == 0) g_odata[0] = sdata[0];
+}
+
+// 改进 改进 消除shared memory bank 访问冲突
+__global__ void reduce2(FLOAT* g_idata, FLOAT* g_odata) {
+    __shared__ FLOAT sdata[256];
+
+    // 
+    unsigned int tid = threadIdx.x;
+    unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    sdata[tid] = g_idata[i];
+    printf(">>>reduce2 tid=%d\n", tid);
+    __syncthreads();
+
+    // do reduction in shared mem
+    for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1)
+    {
+        if (tid < s)
+        {
+            sdata[tid] += sdata[tid + s];
+            printf(">>>reduce0 tid=%d,sdata[tid]=%f,sdata[tid + s]=%f,\n", tid, sdata[tid], sdata[tid + s]);
+        }
+        __syncthreads();
+    }
+
+    // write result for this block to global mem
+    if (tid == 0) g_odata[0] = sdata[0];
+}
+
+// 改进 
+__global__ void reduce3(FLOAT* g_idata, FLOAT* g_odata) {
+    __shared__ FLOAT sdata[256];
+
+    // 
+    unsigned int tid = threadIdx.x;
+    unsigned int i = blockIdx.x * blockDim.x * 2 + threadIdx.x;
+    sdata[tid] = g_idata[i];
+    printf(">>>reduce2 tid=%d\n", tid);
+    __syncthreads();
+    sdata[tid] = g_idata[i] + g_idata[i + blockDim.x];
+
+    // do reduction in shared mem
+    for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1)
+    {
+        if (tid < s)
+        {
+            sdata[tid] += sdata[tid + s];
+            printf(">>>reduce0 tid=%d,sdata[tid]=%f,sdata[tid + s]=%f,\n", tid, sdata[tid], sdata[tid + s]);
+        }
+        __syncthreads();
+    }
+
+    // write result for this block to global mem
+    if (tid == 0) g_odata[0] = sdata[0];
+}
+
+// 改进 
+__global__ void reduce4(FLOAT* g_idata, FLOAT* g_odata) {
+    __shared__ volatile FLOAT sdata[256];
+
+    // 
+    unsigned int tid = threadIdx.x;
+    unsigned int i = blockIdx.x * blockDim.x * 2 + threadIdx.x;
+    sdata[tid] = g_idata[i];
+    printf(">>>reduce2 tid=%d\n", tid);
+    __syncthreads();
+    sdata[tid] = g_idata[i] + g_idata[i + blockDim.x];
+
+    // do reduction in shared mem
+    for (unsigned int s = blockDim.x / 2; s > 32; s >>= 1)
+    {
+        if (tid < s)
+        {
+            sdata[tid] += sdata[tid + s];
+            printf(">>>reduce0 tid=%d,sdata[tid]=%f,sdata[tid + s]=%f,\n", tid, sdata[tid], sdata[tid + s]);
+        }
+        __syncthreads();
+    }
+    if (tid < 32)
+    {
+        sdata[tid] += sdata[tid + 32];
+        sdata[tid] += sdata[tid + 16];
+        sdata[tid] += sdata[tid + 8];
+        sdata[tid] += sdata[tid + 4];
+        sdata[tid] += sdata[tid + 2];
+        sdata[tid] += sdata[tid + 1];
+    }
+
+    // write result for this block to global mem
+    if (tid == 0) g_odata[0] = sdata[0];
+}
 
 /* sum all entries in x and asign to y */
 __device__ void warpReduce(volatile FLOAT *sdata, int tid)
@@ -100,6 +239,13 @@ __global__ void VecSumKnl_v1(const FLOAT *x, FLOAT *y)
     }
 }
 
+__global__ void warm_up_gpu() {
+    unsigned int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    float ia, ib;
+    ia = ib = 0.0f;
+    ib += ia + tid;
+}
+
 int reduction_1B_v1_06()
 {
     int N = 256;   /* must be 256 */
@@ -134,12 +280,15 @@ int reduction_1B_v1_06()
     for (i = 0; i < N; i++) {
         hx[i] = 1;
     }
-
+    warm_up_gpu << <1, N >> > ();
+    cudaDeviceSynchronize();
     /* copy data to GPU */
     cudaMemcpy(dx, hx, nbytes, cudaMemcpyHostToDevice);
 
     /* call GPU */
-    VecSumKnl_v1 <<<1, N>>>(dx, dy);
+    //reduce2 <<<1, N>>>(dx, dy);
+    reduce3 <<<1, N/2>>>(dx, dy);
+    //VecSumKnl_v1 <<<1, N>>>(dx, dy);
 
     /* let GPU finish */
     cudaDeviceSynchronize();
