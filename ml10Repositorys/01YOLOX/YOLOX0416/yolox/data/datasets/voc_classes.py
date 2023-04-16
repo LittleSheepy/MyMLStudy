806391b728f7afedbc8a1e5b9a9f2ea0e902a6e7
LittleSheepy <1165811567@qq.com>
2023-04-16 22:55:40 +0800

    1

---
+++ ml10Repositorys/01YOLOX/YOLOX0416/yolox/data/datasets/voc_classes.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+
+# VOC_CLASSES = ( '__background__', # always index 0
+VOC_CLASSES = (
+    "bd", "bb", "gh", "wb", "wc", "ps", "yqwrw", "zx"
+)
+# VOC_CLASSES = (
+#     "qipao",
+# )
+# VOC_CLASSES = (
+#     "aeroplane",
+#     "bicycle",
+#     "bird",
+#     "boat",
+#     "bottle",
+#     "bus",
+#     "car",
+#     "cat",
+#     "chair",
+#     "cow",
+#     "diningtable",
+#     "dog",
+#     "horse",
+#     "motorbike",
+#     "person",
+#     "pottedplant",
+#     "sheep",
+#     "sofa",
+#     "train",
+#     "tvmonitor",
+# )
