4d3a9c4c7b3edbd725d04c37b20a8c1f3bf1ef2d
LittleSheepy <1165811567@qq.com>
2023-04-18 23:19:55 +0800

    1
    1

---
+++ ml06pyPackeg/pk10sklearn/sk_datasets/ds00main.py
@@ -0,0 +1,6 @@
+from ds01创建数据集 import my_make_circles       # 创建两个同心圆
+
+
+
+if __name__ == '__main__':
+    my_make_circles()
---
+++ ml06pyPackeg/pk10sklearn/sk_datasets/ds01创建数据集.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import make_circles
+
+"""
+factor: 内圈和外圈半径之比
+"""
+# 创建两个同心圆
+def my_make_circles():
+    X, y = make_circles(n_samples=100, noise=0.05, factor=0.3, shuffle=False)
+    X1 = X[y == 0]
+    X2 = X[y == 1]
+
+    fig, ax = plt.subplots()
+    ax.scatter(X1[:, 0], X1[:, 1], color='blue', label='set 1')
+    ax.scatter(X2[:, 0], X2[:, 1], color='orange', label='set 2')
+    ax.legend(loc=0)
+    plt.show()
+
