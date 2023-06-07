c45932bf2b304ebfb9cbcc84fae2bf78b1561679
LittleSheepy <38182200+LittleSheepy@users.noreply.github.com>
2023-06-07 21:10:51 +0800

    111

--- data/pic_move.py
+++ data/pic_move.py
@@ -2,24 +2,24 @@ import os, shutil
 
 
 
-
-
-
-
 def move1():
-    img_dir = "D:/01sheepy/01work/01baojie_ocr/pp/img/"
-    biaozhu = "D:/01sheepy/01work/01baojie_ocr/pp/img_biaozhu/"
+    root_dir = r"D:\03GitHub\aFolder_YOLO\03yolov8s\ultralytics\runs\detect/predict11/"
+    root_dir = r"D:\03GitHub\aFolder_YOLO\03yolov8s\ultralytics\runs\detect\predict14\/"
+    root_dir = r"D:\0\0LG_DATA\SZ_NG_0529\black\/"
+    img_dir = root_dir + r"\img_save2_1/"
+    biaozhu = root_dir + "/img_have0606/"
     nobiaozhu = "D:/01sheepy/01work/01baojie_ocr/pp/img_nobiaozhu/"
-    xml_dir = "D:/01sheepy/01work/01baojie_ocr/pp/xml/"
+    xml_dir = root_dir + "txt_have0606/"
     for imgfile in os.listdir(img_dir):
         moveFlg = False
         for xmlfile in os.listdir(xml_dir):
             print(xmlfile)
             if xmlfile[:-4] == imgfile[:-4]:
-                shutil.copyfile(img_dir + imgfile, biaozhu + imgfile)
+                # shutil.copyfile(img_dir + imgfile, biaozhu + imgfile)
+                shutil.move(img_dir + imgfile, biaozhu + imgfile)
                 moveFlg = True
-        if not moveFlg:
-            shutil.copyfile(img_dir + imgfile, nobiaozhu + imgfile)
+        # if not moveFlg:
+        #     shutil.copyfile(img_dir + imgfile, nobiaozhu + imgfile)
 
 
 
