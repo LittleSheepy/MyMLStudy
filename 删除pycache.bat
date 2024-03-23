56d51d84a1674c7869ac66a365c5740cd8449527
LittleSheepy <1165811567@qq.com>
2024-03-24 04:34:08 +0800

    1

--- 删除pycache.bat
+++ 删除pycache.bat
@@ -1,2 +1,5 @@
 for /d /r . %%d in (__pycache__) do @if exist "%%d" echo "%%d" && rd /s /q "%%d"
+for /d /r . %%d in (.vs) do @if exist "%%d" echo "%%d" && rd /s /q "%%d"
+for /d /r . %%d in (bin) do @if exist "%%d" echo "%%d" && rd /s /q "%%d"
+for /d /r . %%d in (build) do @if exist "%%d" echo "%%d" && rd /s /q "%%d"
 pause
