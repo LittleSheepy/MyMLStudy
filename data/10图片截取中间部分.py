2ba98077ffa2351e622cab3d8b3fbee3f1344c3a
LittleSheepy <38182200+LittleSheepy@users.noreply.github.com>
2024-07-19 18:34:35 +0800

    1
    1

--- data/10图片截取中间部分.py
+++ data/10图片截取中间部分.py
@@ -4,10 +4,15 @@ from PIL import Image
 
 def crop_center(image, crop_size):
     w, h = image.size
-    left = (w - crop_size) // 2
-    top = (h - crop_size) // 2
-    right = (w + crop_size) // 2
-    bottom = (h + crop_size) // 2
+    left = top = 0
+    right = w
+    bottom = h
+    if w > crop_size:
+        left = (w - crop_size) // 2
+        right = (w + crop_size) // 2
+    if h > crop_size:
+        top = (h - crop_size) // 2
+        bottom = (h + crop_size) // 2
     return image.crop((left, top, right, bottom))
 
 
@@ -21,7 +26,7 @@ def process_images(input_folder, output_folder, crop_size=500):
 
         if img is not None:
             w, h = img.size
-            if h >= crop_size and w >= crop_size:
+            if h >= crop_size or w >= crop_size:
                 cropped_img = crop_center(img, crop_size)
                 output_path = os.path.join(output_folder, filename)
                 cropped_img.save(output_path)
@@ -30,8 +35,8 @@ if __name__ == '__main__':
     # dir_root = r"D:\02dataset\01work\09DGKaiDe\00imgAll/"
     dir_root = r"./"
     # dir_root = r"D:\02dataset\01work\07HZHengTai\00imgAll1\01ZhengJi/"
-    dir_root = r"D:\02dataset\01work\09DGKaiDe\00imgAll\06TaiGun_result\/"
+    dir_root = r"D:\02dataset\01work\07HZHengTai\01zip\02负极\负极瑕疵7.19\负极瑕疵7.19\/"
 
-    input_folder = dir_root + '03/'
-    output_folder = dir_root + '00_500/'
+    input_folder = dir_root + '褶皱/'
+    output_folder = dir_root + '褶皱_500/'
     process_images(input_folder, output_folder)
