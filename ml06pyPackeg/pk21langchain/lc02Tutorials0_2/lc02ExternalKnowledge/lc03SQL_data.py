db83f2de60ccf40bb7734daa90377b19d8a3009a
LittleSheepy <38182200+LittleSheepy@users.noreply.github.com>
2024-08-02 20:00:27 +0800

    1
    1

--- ml06pyPackeg/pk21langchain/lc02Tutorials0_2/lc02ExternalKnowledge/lc03SQL_data.py
+++ ml06pyPackeg/pk21langchain/lc02Tutorials0_2/lc02ExternalKnowledge/lc03SQL_data.py
@@ -19,38 +19,205 @@ llm = Tongyi()
 
 
 
+from langchain_community.utilities import SQLDatabase
+
+db = SQLDatabase.from_uri("sqlite:///Chinook.db")
+print(db.dialect)
+print(db.get_usable_table_names())
+db.run("SELECT * FROM Artist LIMIT 10;")
+
+from langchain.chains import create_sql_query_chain
+
+chain = create_sql_query_chain(llm, db)
+response = chain.invoke({"question": "How many employees are there"})
+print(response)             # SELECT COUNT(*) FROM "Employee"
+
+result = db.run(response)
+print(result)               # '[(8,)]'
+
+chain.get_prompts()[0].pretty_print()
+"""
+你是一个SQLite专家。给定一个输入问题，首先创建一个语法正确的SQlite查询来运行，然后查看查询的结果并返回输入问题的答案。
+除非用户在问题中指定要获取的特定数量的示例，否则按照sQlite的要求，使用LIMIT子句最多查询5个结果。
+您可以对结果进行排序，以返回数据库中信息最丰富的数据。
+永远不要查询表中的所有列。您必须只查询回答问题所需的列。
+将每个列名用双引号(")括起来，表示为分隔的标识符。
+注意，只使用您可以在下面的表中看到的列名。
+注意不要查询不存在的列。另外，要注意哪个列在哪个表中。
+注意使用date('now')函数来获取当前日期，如果问题涉及“今天”。
+使用如下格式:
+Question: Question here
+SQLQuery: SQLQuery to rul
+SQLResult: SQLQuery的结果
+Answer:最终答案here
+只使用以下表:
+{table_info}
+Question: {input}
+
+You are a SQLite expert. Given an input question, first create a syntactically correct SQLite query to run, then look at the results of the query and return the answer to the input question.
+Unless the user specifies in the question a specific number of examples to obtain, query for at most 5 results using the LIMIT clause as per SQLite. 
+You can order the results to return the most informative data in the database.
+Never query for all columns from a table. You must query only the columns that are needed to answer the question. 
+Wrap each column name in double quotes (") to denote them as delimited identifiers.
+Pay attention to use only the column names you can see in the tables below. 
+Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
+Pay attention to use date('now') function to get the current date, if the question involves "today".
+
+Use the following format:
+
+Question: Question here
+SQLQuery: SQL Query to run
+SQLResult: Result of the SQLQuery
+Answer: Final answer here
+
+Only use the following tables:
+{table_info}
+
+Question: {input}
+"""
+
+from langchain_community.tools.sql_database.tool import QuerySQLDataBaseTool
+
+execute_query = QuerySQLDataBaseTool(db=db)
+write_query = create_sql_query_chain(llm, db)
+chain = write_query | execute_query
+response = chain.invoke({"question": "How many employees are there"})
+print(response)               # '[(8,)]'
 
 
+"""
+    回答问题
+"""
+from operator import itemgetter
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+
+answer_prompt = PromptTemplate.from_template(
+    """Given the following user question, corresponding SQL query, and SQL result, answer the user question.
+
+Question: {question}
+SQL Query: {query}
+SQL Result: {result}
+Answer: """
+)
+
+chain = (
+    RunnablePassthrough.assign(query=write_query).assign(
+        result=itemgetter("query") | execute_query
+    )
+    | answer_prompt
+    | llm
+    | StrOutputParser()
+)
+
+response = chain.invoke({"question": "How many employees are there"})
+print(response)               # '[(8,)]'
 
 
+from langchain_community.agent_toolkits import SQLDatabaseToolkit
+
+toolkit = SQLDatabaseToolkit(db=db, llm=llm)
+
+tools = toolkit.get_tools()
+
+print(tools)
+
+from langchain_core.messages import SystemMessage
+
+SQL_PREFIX = """You are an agent designed to interact with a SQL database.
+Given an input question, create a syntactically correct SQLite query to run, then look at the results of the query and return the answer.
+Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most 5 results.
+You can order the results by a relevant column to return the most interesting examples in the database.
+Never query for all the columns from a specific table, only ask for the relevant columns given the question.
+You have access to tools for interacting with the database.
+Only use the below tools. Only use the information returned by the below tools to construct your final answer.
+You MUST double check your query before executing it. If you get an error while executing a query, rewrite the query and try again.
+
+DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
+
+To start you should ALWAYS look at the tables in the database to see what you can query.
+Do NOT skip this step.
+Then you should query the schema of the most relevant tables."""
+
+system_message = SystemMessage(content=SQL_PREFIX)
+
+from langchain_core.messages import HumanMessage
+from langgraph.prebuilt import create_react_agent
+
+agent_executor = create_react_agent(llm, tools, messages_modifier=system_message)
+
+for s in agent_executor.stream(
+    {"messages": [HumanMessage(content="Which country's customers spent the most?")]}
+):
+    print(s)
+    print("----")
+
+for s in agent_executor.stream(
+    {"messages": [HumanMessage(content="Describe the playlisttrack table")]}
+):
+    print(s)
+    print("----")
+
+import ast
+import re
 
 
+def query_as_list(db, query):
+    res = db.run(query)
+    res = [el for sub in ast.literal_eval(res) for el in sub if el]
+    res = [re.sub(r"\b\d+\b", "", string).strip() for string in res]
+    return list(set(res))
 
 
+artists = query_as_list(db, "SELECT Name FROM Artist")
+albums = query_as_list(db, "SELECT Title FROM Album")
+print(albums[:5])
+from langchain.agents.agent_toolkits import create_retriever_tool
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings
 
+vector_db = FAISS.from_texts(artists + albums, OpenAIEmbeddings())
+retriever = vector_db.as_retriever(search_kwargs={"k": 5})
+description = """Use to look up values to filter on. Input is an approximate spelling of the proper noun, output is \
+valid proper nouns. Use the noun most similar to the search."""
+retriever_tool = create_retriever_tool(
+    retriever,
+    name="search_proper_nouns",
+    description=description,
+)
+print(retriever_tool.invoke("Alice Chains"))
 
+system = """You are an agent designed to interact with a SQL database.
+Given an input question, create a syntactically correct SQLite query to run, then look at the results of the query and return the answer.
+Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most 5 results.
+You can order the results by a relevant column to return the most interesting examples in the database.
+Never query for all the columns from a specific table, only ask for the relevant columns given the question.
+You have access to tools for interacting with the database.
+Only use the given tools. Only use the information returned by the tools to construct your final answer.
+You MUST double check your query before executing it. If you get an error while executing a query, rewrite the query and try again.
 
+DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
 
+You have access to the following tables: {table_names}
 
+If you need to filter on a proper noun, you must ALWAYS first look up the filter value using the "search_proper_nouns" tool!
+Do not try to guess at the proper name - use this function to find similar ones.""".format(
+    table_names=db.get_usable_table_names()
+)
 
+system_message = SystemMessage(content=system)
 
+tools.append(retriever_tool)
 
+agent = create_react_agent(llm, tools, messages_modifier=system_message)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for s in agent.stream(
+    {"messages": [HumanMessage(content="How many albums does alis in chain have?")]}
+):
+    print(s)
+    print("----")
 
 
 
