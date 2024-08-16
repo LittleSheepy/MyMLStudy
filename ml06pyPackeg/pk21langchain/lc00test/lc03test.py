501888512e15920f2aab8910d028fb215d59e73d
LittleSheepy <38182200+LittleSheepy@users.noreply.github.com>
2024-08-16 20:04:03 +0800

    1
    1

--- ml06pyPackeg/pk21langchain/lc00test/lc03test.py
+++ ml06pyPackeg/pk21langchain/lc00test/lc03test.py
@@ -6,8 +6,5 @@ from langchain_community.llms import QianfanLLMEndpoint
 print(langchain.__version__)
 os.environ["DASHSCOPE_API_KEY"] = "sk-720b1666b12f49c3915e4061e173ab15"
 
-
-
 llm_tongyi = Tongyi()
-llm_qianfan = QianfanLLMEndpoint(temperature=0.1)
-print(llm_qianfan.invoke("1+1等于几"))
+print(llm_tongyi.invoke("1+1等于几"))
